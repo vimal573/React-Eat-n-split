639f7306d0bc3d6ceb3ded6b0a53daeea35c6f50
Vimal Kumar <Vimal2540.vk@gmail.com>
2024-07-06 09:34:02 +0530

    FreindsList and Friends Coompnent created

--- src/App.js
+++ src/App.js
@@ -19,12 +19,47 @@ const initialFriends = [
   },
 ];
 
-function App() {
+export default function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
     </div>
   );
 }
 
-export default App;
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map(friend => (
+        <Friend friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owe you {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are Even</p>}
+
+      <button className="button">select</button>
+    </li>
+  );
+}
